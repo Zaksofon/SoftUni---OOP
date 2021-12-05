3cf76775f8feb7de85814c8fdbbd618d3e279085
Zaksofon <81183855+Zaksofon@users.noreply.github.com>
2021-12-05 11:00:33 +0200

    Add files via upload

---
+++ OOP Exercises/Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;  
+        }
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get
+            {
+                return age;     
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative!");
+                }
+
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
